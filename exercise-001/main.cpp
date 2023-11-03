504661647c7cdb6000f878182ac21bbba0385c5a
stefan-dir <149794541+stefan-dir@users.noreply.github.com>
2023-11-03 15:35:55 +0100

    added stackvariable

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -16,6 +16,7 @@ int data = 4711;
 // .rodata segment
 const int rodata = 42;
 
+
 auto main(int argc, char **argv) -> int
 {
     /**
@@ -24,9 +25,19 @@ auto main(int argc, char **argv) -> int
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {}!\n", "hah");
+    
+    // .stack segment
+    
+    int stack2;
+    int stack = 5;
+    
 
     fmt::print("Value of Variable bss {} address of variable bss {}\n", foo, fmt::ptr(&foo));
     fmt::print("Value of Variable data {} address of variable bss {}\n", data, fmt::ptr(&data));
     fmt::print("Value of Variable rodata {} address of variable rodata {}\n", rodata, fmt::ptr(&rodata));
+    fmt::print("Value of Variable stack {} address of variable stack {}\n", stack, fmt::ptr(&stack));
+    fmt::print("Value of Variable stack2 {} address of variable stack2 {}\n", stack2, fmt::ptr(&stack2));
+
+
     return 0; /* exit gracefully*/
 }
