c3b282c56cf76fe48fc9b231f79200533874402f
Christian Ege <ch@ege.io>
2023-11-10 10:18:53 +0100

    feat: add counter

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -6,6 +6,7 @@
 
 auto main(int argc, char **argv) -> int
 {
+    auto count = 20;
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
@@ -14,13 +15,18 @@ auto main(int argc, char **argv) -> int
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-c,--count",
+            count, 
+            fmt::format("Create a vector with the given size default: {}", count));
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
     }
+    std::vector<int> data(count);
 
+    fmt::print("Created a vector with {} elements\n",data.size());
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
