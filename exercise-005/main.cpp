55238c81938e7a74fc9a35d9e8cff12ef1d28b6e
Christian Ege <ch@ege.io>
2023-12-01 16:48:31 +0100

    feat: add the table based solution

--- exercise-005/main.cpp
+++ exercise-005/main.cpp
@@ -4,6 +4,126 @@
 #include "CLI/CLI.hpp"
 #include "config.h"
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * The inernal states
+ */
+typedef enum {
+    STATE_OBEN         = 0,
+    STATE_UNTEN        = 1,
+    STATE_FAHRE_HOCH   = 2,
+    STATE_FAHRE_RUNTER = 3,
+    STATE_STOP_HOCH    = 4,
+    STATE_STOP_RUNTER  = 5,
+    STATE_ERROR        = 6,
+    STATE_MAX
+} StatemachineState_t;
+
+typedef enum {
+    EV_TASTE               = 0,
+    EV_ENDSCHALTER_OBEN    = 1,
+    EV_ENDSCHALTER_UNTEN   = 2,
+    EV_MAX
+} Event_t;
+
+
+int error(Event_t e)
+{
+    fmt::println("Error detected! Event: {}",(int)e);
+    return EXIT_FAILURE;
+}
+
+int state_oben(Event_t e)
+{
+    fmt::println("Aktueller Zustand: OBEN Event: {}",(int)e);
+    return EXIT_SUCCESS;
+}
+
+int state_unten(Event_t e)
+{
+    fmt::println("Aktueller Zustand: UNTEN Event: {}",(int)e);
+    return EXIT_SUCCESS;
+}
+
+int state_fahre_hoch(Event_t e)
+{
+    fmt::println("Aktueller Zustand: FAHRE_HOCH Event: {}",(int)e);
+    return EXIT_SUCCESS;
+}
+
+int state_fahre_runter(Event_t e)
+{
+    fmt::println("Aktueller Zustand: FAHRE_RUNTER Event: {}",(int)e);
+    return EXIT_SUCCESS;
+}
+
+int state_stop_runter(Event_t e)
+{
+    fmt::println("Aktueller Zustand: STOP_RUNTER Event: {}",(int)e);
+    return EXIT_SUCCESS;
+}
+
+int state_stop_hoch(Event_t e)
+{
+    fmt::println("Aktueller Zustand: STOP_HOCH Event: {}",(int)e);
+    return EXIT_SUCCESS;
+}
+
+typedef int (*stateHandler_t)(Event_t e);
+
+typedef struct {
+    StatemachineState_t nextState;
+    stateHandler_t hdl;
+}StateTuple_t;
+
+typedef struct MyState{
+    StatemachineState_t state; /*< The state   */
+} MyState_t;
+
+
+
+static StateTuple_t TransitionTable[EV_MAX][STATE_MAX] = {
+    /*                             STATE_OBEN                                STATE_UNTEN                           STATE_FAHRE_HOCH                          STATE_FAHRE_RUNTER                     STATE_STOP_HOCH                          STATE_STOP_RUNTER */ 
+    /* EV_TASTE              */ { {STATE_FAHRE_RUNTER,state_fahre_runter},  {STATE_FAHRE_HOCH,state_fahre_hoch},  {STATE_STOP_HOCH,state_stop_hoch},        {STATE_STOP_RUNTER,state_stop_runter}, {STATE_FAHRE_RUNTER,state_fahre_runter}, {STATE_FAHRE_HOCH,state_fahre_hoch} },
+    /* EV_ENDSCHALTER_OBEN   */ { {STATE_OBEN,state_oben},                  {STATE_ERROR,error},                  {STATE_OBEN,state_oben},                  {STATE_ERROR,error},                   {STATE_ERROR,error},                     {STATE_ERROR,error},                },
+    /* EV_ENDSCHALTER_UNTEN  */ { {STATE_ERROR,error},                      {STATE_UNTEN,state_unten},            {STATE_ERROR,error},                      {STATE_UNTEN,state_unten},             {STATE_ERROR,error},                     {STATE_ERROR,error},                }
+};
+
+
+MyState_t* myStatemachine_create(void)
+{
+    MyState_t* ret = (MyState_t*)malloc(sizeof(MyState_t));
+    if(NULL != ret)
+    {
+        memset(ret,0,sizeof(MyState_t));
+        ret->state = STATE_FAHRE_HOCH;
+    }
+    return ret;
+}
+
+void myStatemachine_free(MyState_t* statemachine)
+{
+    if(NULL != statemachine)
+    {
+        free(statemachine);
+    }
+}
+
+int myStatemachine_handle(MyState_t* statemachine, Event_t event)
+{
+    int ret = 1;
+    if(   ((STATE_OBEN <= statemachine->state) && (STATE_MAX > statemachine->state)))
+    {
+        StateTuple_t state = TransitionTable[event][statemachine->state];
+        statemachine->state = state.nextState;
+        ret = state.hdl(event);
+    }
+    return ret;
+}
+
 auto main(int argc, char **argv) -> int
 {
     /**
@@ -28,7 +148,23 @@ auto main(int argc, char **argv) -> int
      */
     fmt::print("Hello, {}!\n", app.get_name());
 
-    /* INSERT YOUR CODE HERE */
+        Event_t test[] = { EV_ENDSCHALTER_OBEN,
+        EV_TASTE,
+        EV_TASTE,
+        EV_TASTE,
+        EV_ENDSCHALTER_OBEN,
+    };
+    int i = 0;
+
+    MyState_t* machine = myStatemachine_create();
+
+    for(i = 0; i < sizeof(test)/sizeof(Event_t); i++)
+    {
+        myStatemachine_handle(machine,test[i]);
+    }
+
+    myStatemachine_free(machine);
+    return 0;
 
     return 0; /* exit gracefully*/
 }
